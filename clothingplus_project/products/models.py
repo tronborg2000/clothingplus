b8db0eb3ff3e45047889a233249d252d71f44b4d
tronborg2000 <colinweldon1980@gmail.com>
2023-06-22 09:53:17 +0000

    Added 'products' and 'rest_framework' to INSTALLED_APPS in settings.py

--- clothingplus_project/products/models.py
+++ clothingplus_project/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # ... add more fields as required
+
+    def __str__(self):
+        return self.title
--- clothingplus_project/settings.py
+++ clothingplus_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',  # Add this line
+    'rest_framework',  # Add this line as well
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
