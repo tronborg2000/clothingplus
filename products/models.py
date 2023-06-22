f1674cc8e8c860c571c970764da43eda858e09c4
tronborg2000 <colinweldon1980@gmail.com>
2023-06-22 10:09:49 +0000

    Defined Product, Comment, and Vote models

--- products/models.py
+++ products/models.py
@@ -1,11 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    # ... add more fields as required
+    url = models.URLField()
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.user)
+
+class Vote(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='votes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Vote for product {} by {}'.format(self.product, self.user)
+
