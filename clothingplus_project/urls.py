a142d6fab748a4a4d6841d773f48b7b44ff10b84
tronborg2000 <colinweldon1980@gmail.com>
2023-06-29 09:34:57 +0000

    update

--- clothingplus_project/urls.py
+++ clothingplus_project/urls.py
@@ -1,8 +1,14 @@
 from django.contrib.auth import views as auth_views
-from products.views import register
 from django.contrib import admin
 from django.urls import path
-from products.views import product_list, product_detail, product_create, upvote_product
+from products.views import (
+    product_list,
+    product_detail,
+    product_create,
+    upvote_product,
+    product_search,
+)
+from products.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +17,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('search/', product_search, name='product_search'),
-
+    path('products/', product_list, name='product_list'),
+    path('products/create/', product_create, name='product_create'),
+    path('products/<int:pk>/', product_detail, name='product_detail'),
 ]
-
